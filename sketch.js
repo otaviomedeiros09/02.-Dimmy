e51e6dbb117da8fc113d71cac8647d745e8709cb
otaviomedeiros09 <otavio2006medeiros10@gmail.com>
2024-05-29 10:29:18 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(600,600);
+  background("black");
+}
+
+function draw() {
+
+  stroke("blue");
+  fill("red")
   
-  function draw() {
-    background(220);
-  }
+  if(mouseIsPressed){
+    rect(mouseX,mouseY,20, 35);
   
+}
+  }
